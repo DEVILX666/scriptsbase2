23bafa690405fc2a36f643a4ef3ec6dc64f75e6d
DEVILX666 <batatabatatax5@gmail.com>
2026-06-17 14:42:43 +0100

    Update video-overlay.tsx

--- components/video-overlay.tsx
+++ components/video-overlay.tsx
@@ -93,7 +93,7 @@ export function VideoOverlay({ isOpen, onClose, onContinue, lockerUrl, gameName 
     if (!isOpen) return
 
     setIsButtonEnabled(false)
-    setTimeRemaining(190)
+    setTimeRemaining(1)
     
     timerRef.current = setInterval(() => {
       setTimeRemaining((prev) => {
